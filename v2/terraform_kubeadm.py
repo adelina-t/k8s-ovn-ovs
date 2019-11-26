3710e0709b84c43618464944e714c65ed0c41c41
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-11-26 17:59:12 +0200

    Delete sleep in up() for terraform_kubeadm

--- v2/terraform_kubeadm.py
+++ v2/terraform_kubeadm.py
@@ -33,8 +33,6 @@ class Terraform_Kubeadm(ci.CI):
         self.logging.info("Bringing cluster up.")
         try:
             self.deployer.up()
-            import time
-            time.sleep(1000000000)
         except Exception as e:
             raise e
 
@@ -53,7 +51,7 @@ class Terraform_Kubeadm(ci.CI):
 
     def _build_k8s_bins(self):
         build_host = self.deployer.get_cluster_master_public_ip()
-        remote_k8s_path = "/home/ubuntu/"
+        remote_k8s_path = "/home/ubuntu/kubernetes"
         # For this scenario, we need docker to build k8s, thus all building should happen on remote master host
         
         k8s_linux_bins = ["kubectl", "kubelet", "kubeadm"]
@@ -66,20 +64,20 @@ class Terraform_Kubeadm(ci.CI):
             #Binaries built here are to be installed dirrectly on the master, and are not built by the _build_k8s_release_images function
             for binary in k8s_linux_bins:
                 cmd = "cd kubernetes ; build/run.sh make %s KUBE_BUILD_PLATFORMS=linux/amd64" % binary
-                self._runRemoteCmd(cmd, build_host)
+                self._runRemoteCmd(cmd, [build_host])
 
         def _build_k8s_windows_binaries():
             for binary in k8s_linux_bins:
                 cmd = "cd kubernetes ; build/run.sh make %s KUBE_BUILD_PLATFORMS=windows/amd64" % binary
-                self._runRemoteCmd(cmd, build_host)
+                self._runRemoteCmd(cmd, [build_host])
 
         def _package_k8s_windows_binaries():
             cmd = "cd kubernetes ; mkdir _output/release-tars ; KUBE_BUILD_PLATFORMS=windows/amd64 && TAR=\`which tar\` && source build/common.sh && source build/lib/release.sh && kube::release::package_src_tarball && kube::release::package_node_tarballs "
-            self._runRemoteCmd(cmd, build_host)
+            self._runRemoteCmd(cmd, [build_host])
 
         def _build_k8s_release_images():
             cmd = "cd kubernetes ; export KUBE_VERBOSE=0 ; export KUBE_BUILD_CONFORMANCE=n ; export KUBE_BUILD_PLATFORMS=linux/amd64; export KUBE_BUILD_HYPERKUBE=n ;  make quick-release-images"
-            self._runRemoteCmd(cmd, build_host)
+            self._runRemoteCmd(cmd, [build_host])
         
         self._clone_k8s_repo(build_host, remote_k8s_path)
         _install_prereqs()
@@ -90,17 +88,17 @@ class Terraform_Kubeadm(ci.CI):
 
     def _clone_k8s_repo(self, remote_host, remote_path):
         cmd = utils.get_clone_command(self.opts.k8s_repo, self.opts.k8s_branch, remote_path)
-        self._runRemoteCmd(" ".join(cmd), remote_host)
+        self._runRemoteCmd(" ".join(cmd), [remote_host])
 
     # These functions are implemented like so to preserve the interface defined by collectlogs functions
     # Those functions should be moved to base CI class and they should be used by all children.
-    def _runRemoteCmd(self, command, machines, retries, windows=False, root=False):
+    def _runRemoteCmd(self, command, machines, retries=0, windows=False, root=False):
         if windows:
             username = self.deployer.get_win_vm_username()
         else:
             username = self.default_linux_username
         for machine in machines:
-            out = utils.run_ssh_cmd([command], username, machine, self.opts.ssh_private_key_path)
+            out = utils.run_ssh_cmd(["\"%s\"" % command], username, machine, self.opts.ssh_private_key_path)
 
     def _copyTo(self, src, dest, machines, windows=False, root=False):
         if windows:
